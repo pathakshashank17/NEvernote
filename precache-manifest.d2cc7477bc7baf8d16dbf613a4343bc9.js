b5114ba01f02554a6e117123d6b7c4084d2ed7a8
Shashank Pathak <pathakshashank32414@gmail.com>
2020-07-16 14:46:47 +0530

    Updates

---
+++ precache-manifest.d2cc7477bc7baf8d16dbf613a4343bc9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d43125abe8c4ed8596c23a501e48fe4",
+    "url": "/NEvernote/index.html"
+  },
+  {
+    "revision": "0f2e9c83d43940adb6b7",
+    "url": "/NEvernote/static/js/2.3c966012.chunk.js"
+  },
+  {
+    "revision": "b27b240a54596cf9b381",
+    "url": "/NEvernote/static/js/main.92d4c9d6.chunk.js"
+  },
+  {
+    "revision": "bf1b34da52b6de388aa1",
+    "url": "/NEvernote/static/js/runtime-main.cc96bfa8.js"
+  }
+]);
